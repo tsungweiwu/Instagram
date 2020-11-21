2ea0e8c20e8253b54f83e792f51f7787c3f186c2
Tsung Wei Wu <tsungweiwu@gmail.com>
2020-11-20 19:25:08 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Tsung Wei Wu on 11/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
